14cd0ab9ac77191c85b21ceff32c348b70344412
Deni Gunawan <artdengun@gmail.com>
2020-10-30 14:12:53 +0700

    Sistem Informasi Akademik

---
+++ src/main/java/com/deni/Sistem/Informasi/Akademik/SistemInformasiAkademikApplication.java
@@ -0,0 +1,13 @@
+package com.deni.Sistem.Informasi.Akademik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemInformasiAkademikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemInformasiAkademikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deni/Sistem/Informasi/Akademik/SistemInformasiAkademikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deni.Sistem.Informasi.Akademik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemInformasiAkademikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
